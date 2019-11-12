1a30103eeaa4adff14ad99f7ee18c0e76314ca88
raritetmolodoy <tuistssip@gmail.com>
2019-11-12 20:58:41 +0500

    added dynamic 'document.title'

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ Vue.use(Router)
 let router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+
   routes: [
     {
       path: '/',
@@ -17,32 +18,35 @@ let router = new Router({
     {
       path: '/login',
       component: () => import('./views/Login'),
-      meta: { requiresGuest: true }
+      meta: { requiresGuest: true, title: 'Вход' }
     },
     {
       path: '/register',
       component: () => import('./views/Register'),
-      meta: { requiresGuest: true }
+      meta: { requiresGuest: true, title: 'Регистрация' }
     },
     {
       path: '/account',
       component: () => import('./views/Account'),
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true, title: 'Мой профиль' }
     },
     {
       path: '/restore',
       component: () => import('./views/RestorePassword'),
-      meta: { requiresGuest: true }
+      meta: { requiresGuest: true, title: 'Восстановление пароля' }
     },
     {
       path: '*',
-      component: () => import('./views/NotFound')
+      component: () => import('./views/NotFound'),
+      meta: { title: 'Ошибка 404' }
     }
   ],
   scrollBehavior: () => ({ x: 0, y: 0 })
 })
 
 router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Scimitar'
+
   const currentUser = firebase.auth().currentUser
   const requiresAuth = to.matched.some(r => r.meta.requiresAuth)
   const requiresGuest = to.matched.some(r => r.meta.requiresGuest)

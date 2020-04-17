2f975d3dd9a1ad40d4226c672d6ca89a36dcfbaa
Timofei <tuistssip@gmail.com>
2020-04-17 23:44:49 +0500

    (groupList): table exported into component

--- src/components/teacher/GroupList.vue
+++ src/components/teacher/GroupList.vue
@@ -29,28 +29,7 @@
           <b-card-body v-if="groups && groups.length">
             <b-link @click="copyLink">Пригласить студентов</b-link>
             <input type="hidden" :id="'invite-link-' + openedGroupIndex" />
-            <b-table
-              :fields="tableHeaders"
-              :items="tableItems"
-              sort-by="name"
-              striped
-              responsive
-              head-variant="light"
-              v-if="marks && tasks"
-              bordered
-              small
-              class="mt-3 marks-table"
-            >
-              <template #head()="data">
-                <div v-b-tooltip.hover="data.field.name">{{ data.label }}</div>
-              </template>
-              <template #cell(index)="data">{{ data.index + 1 }}</template>
-              <template #cell()="data">
-                <div class="score-cell" @click="test(data)">
-                  <div class="score-value">{{data.value}}</div>
-                </div>
-              </template>
-            </b-table>
+            <marks-table :group-index="openedGroupIndex" />
           </b-card-body>
           <b-card-text v-else class="text-center text-muted py-4">Список групп пуст.</b-card-text>
         </b-card>
@@ -65,70 +44,19 @@
 
 <script>
 import { isMobile } from '@/assets/functions'
+import MarksTable from '@/components/teacher/MarksTable'
 import GroupModal from '@/components/modals/GroupModal'
-import { mapState } from 'vuex'
 
 export default {
-  components: { GroupModal },
+  components: { GroupModal, MarksTable },
 
   data: () => ({
     openedGroupIndex: 0
   }),
 
   computed: {
-    ...mapState({
-      groups: s => s.teacher.groups,
-      marks: s => s.teacher.marks,
-      tasks: s => s.teacher.tasks
-    }),
-
-    tableHeaders() {
-      let tableHeaders = [{
-        label: '#',
-        key: 'index'
-      }, {
-        label: 'Фамилия Имя',
-        key: 'name',
-        tdClass: 'font-weight-bold text-nowrap',
-        stickyColumn: true,
-        sortable: true
-      }]
-
-      let labNumbers = [...this.tasks].sort((a, b) => a.number - b.number)
-      labNumbers.forEach(t => tableHeaders.push({
-        label: `Лаб №${t.number}`,
-        key: t.id,
-        name: t.name,
-        score: t.score,
-        sortable: true,
-        thClass: 'hide-sort-icon  text-nowrap' + (t.visible ? '' : ' text-danger'),
-        tdClass: 'hoverable-cell'
-      }))
-
-      tableHeaders.push({
-        label: 'Всего',
-        key: 'total',
-        tdClass: 'font-weight-bold text-center',
-        sortable: true
-      })
-
-      return tableHeaders
-    },
-
-    tableItems() {
-      return this.groups[this.openedGroupIndex].students.map(t => {
-        let row = {
-          name: `${t.surname} ${t.name}`,
-          id: t.id,
-          total: 0
-        }
-        let studentsMarks = this.marks.filter(m => m.student.id === t.id)
-        studentsMarks.forEach(m => {
-          row[m.task.id] = m.score
-          row.total += m.score
-        })
-        return row
-      })
+    groups() {
+      return this.$store.state.teacher.groups
     }
   },
 
@@ -144,74 +72,21 @@ export default {
       window.getSelection().removeAllRanges()
       this.$store.commit('setToastMsg', { msg: 'Ссылка приглашения скопирована!', translate: false })
     },
-    test(data) {
-      let markData = {
-        studentId: data.item.id,
-        taskId: data.field.key,
-        groupId: this.groups[this.openedGroupIndex].id,
-        subjectId: this.$route.params.id,
-        score: data.field.score
-      }
-      this.$store.dispatch('markTask', markData)
-    },
     isMobile: () => isMobile()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-/deep/ {
-  .marks-table {
-    width: max-content;
-    .hoverable-cell {
-      cursor: pointer;
-      transition: background-color 0.09s ease-in-out;
-      position: relative;
-      padding: 0;
-
-      &:hover {
-        background-color: #e9ecef !important;
-      }
-
-      .score-cell {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    }
-  }
-
-  .table.b-table > thead > tr > [aria-sort="none"] {
-    background-size: 0;
-
-    &:hover {
-      background-size: 0.65em 1em;
-    }
-  }
-
-  th.b-table-sticky-column {
-    background-color: #e9ecef !important;
-  }
-
-  .active {
-    transition: background-color 0.25s ease-in-out;
-  }
-  .hover {
-    transition: background-color 0.15s ease-in-out;
-    border-radius: 0.25rem;
-
-    &:hover {
-      background-color: #e9ecef;
-    }
-  }
+.active {
+  transition: background-color 0.25s ease-in-out;
 }
+.hover {
+  transition: background-color 0.15s ease-in-out;
+  border-radius: 0.25rem;
 
-@media screen and (max-width: 768px) {
-  .marks-table {
-    width: 100%;
+  &:hover {
+    background-color: #e9ecef;
   }
 }
 </style>
---
+++ src/components/teacher/MarksTable.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <b-table
+      :fields="tableHeaders"
+      :items="tableItems"
+      sort-by="name"
+      striped
+      responsive
+      head-variant="light"
+      v-if="marks && tasks"
+      bordered
+      small
+      class="mt-3 marks-table"
+    >
+      <template #head()="data">
+        <div v-b-tooltip.hover="data.field.name">{{ data.label }}</div>
+      </template>
+      <template #cell(index)="data">{{ data.index + 1 }}</template>
+      <template #cell()="data">
+        <div class="score-cell" @click="test(data)">
+          <div class="score-value">{{data.value}}</div>
+        </div>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: {
+    groupIndex: {
+      type: Number,
+      required: true
+    }
+  },
+
+  computed: {
+    ...mapState({
+      groups: s => s.teacher.groups,
+      marks: s => s.teacher.marks,
+      tasks: s => s.teacher.tasks
+    }),
+
+    tableHeaders() {
+      let tableHeaders = [{
+        label: '#',
+        key: 'index'
+      }, {
+        label: 'Фамилия Имя',
+        key: 'name',
+        tdClass: 'font-weight-bold text-nowrap',
+        stickyColumn: true,
+        sortable: true
+      }]
+
+      let labNumbers = [...this.tasks].sort((a, b) => a.number - b.number)
+      labNumbers.forEach(t => tableHeaders.push({
+        label: `Лаб №${t.number}`,
+        key: t.id,
+        name: t.name,
+        score: t.score,
+        sortable: true,
+        thClass: 'hide-sort-icon  text-nowrap' + (t.visible ? '' : ' text-danger'),
+        tdClass: 'hoverable-cell'
+      }))
+
+      tableHeaders.push({
+        label: 'Всего',
+        key: 'total',
+        tdClass: 'font-weight-bold text-center',
+        sortable: true
+      })
+
+      return tableHeaders
+    },
+
+    tableItems() {
+      return this.groups[this.groupIndex].students.map(t => {
+        let row = {
+          name: `${t.surname} ${t.name}`,
+          id: t.id,
+          total: 0
+        }
+        let studentsMarks = this.marks.filter(m => m.student.id === t.id)
+        studentsMarks.forEach(m => {
+          row[m.task.id] = m.score
+          row.total += m.score
+        })
+        return row
+      })
+    }
+  },
+
+  methods: {
+    test(data) {
+      let markData = {
+        studentId: data.item.id,
+        taskId: data.field.key,
+        groupId: this.groups[this.groupIndex].id,
+        subjectId: this.$route.params.id,
+        score: data.field.score
+      }
+      this.$store.dispatch('markTask', markData)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+/deep/ {
+  .marks-table {
+    width: max-content;
+    .hoverable-cell {
+      cursor: pointer;
+      transition: background-color 0.09s ease-in-out;
+      position: relative;
+      padding: 0;
+
+      &:hover {
+        background-color: #e9ecef !important;
+      }
+
+      .score-cell {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+  }
+
+  .table.b-table > thead > tr > [aria-sort="none"] {
+    background-size: 0;
+
+    &:hover {
+      background-size: 0.65em 1em;
+    }
+  }
+
+  th.b-table-sticky-column {
+    background-color: #e9ecef !important;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .marks-table {
+    width: 100%;
+  }
+}
+</style>

8a67797f11ea6ce6af61da56d62fa4d2fa44b855
Timofei <tuistssip@gmail.com>
2020-04-24 00:42:12 +0500

    pwa config updated

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   outputDir: 'firebase/dist',
   pwa: {
-    themeColor: '#000'
+    name: 'Dragonfly',
+    themeColor: '#000',
+    manifestOptions: {
+      description:
+        'Application that helps teachers to manage tasks and monitor student performance in university',
+      orientation: 'portrait',
+      background_color: '#fff'
+    }
   }
 }

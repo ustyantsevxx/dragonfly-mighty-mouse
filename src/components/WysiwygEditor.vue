23912e0e757dce5a2cc2b607fef71d938e0104ab
Timofei <tuistssip@gmail.com>
2020-05-10 01:09:14 +0500

    refactor wysiwyg editor

--- src/components/WysiwygEditor.vue
+++ src/components/WysiwygEditor.vue
@@ -4,113 +4,31 @@
       v-if="editable"
       :editor="editor"
       v-slot="{ commands, isActive }"
-      class="mb-2"
     >
       <div class="menubar">
         <b-btn-toolbar>
           <b-container fluid class="px-0">
             <b-row no-gutters>
-              <b-col md="auto">
-                <b-btn-group size="sm" class="mr-2">
-                  <b-btn
-                    :class="{ active: isActive.paragraph() }"
-                    @click="commands.paragraph"
-                  >
-                    Абзац
-                  </b-btn>
-                </b-btn-group>
-
-                <b-btn-group size="sm" class="mr-1">
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Полужирный"
-                    :class="{ active: isActive.bold() }"
-                    @click="commands.bold"
-                  >
-                    <b-icon icon="type-bold" />
-                  </b-btn>
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Курсив"
-                    :class="{ active: isActive.italic() }"
-                    @click="commands.italic"
-                  >
-                    <b-icon icon="type-italic" />
-                  </b-btn>
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Подчеркнутый"
-                    :class="{ active: isActive.underline() }"
-                    @click="commands.underline"
-                  >
-                    <b-icon icon="type-underline" />
-                  </b-btn>
-                </b-btn-group>
-
-                <b-btn-group size="sm" class="ml-1 mr-2">
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Код"
-                    :class="{ active: isActive.code() }"
-                    @click="commands.code"
-                  >
-                    <b-icon icon="code-slash" />
-                  </b-btn>
-                </b-btn-group>
-              </b-col>
-              <b-col md="auto" class="second-icon-row">
+              <b-col
+                cols="auto"
+                class="mr-2 mb-2"
+                v-for="(group, i) in buttons"
+                :key="i"
+              >
                 <b-btn-group size="sm">
                   <b-btn
+                    v-for="(btn, j) in group"
+                    :key="j"
                     v-b-tooltip.hover
-                    title="Заголовок"
-                    :class="{ active: isActive.heading({ level: 4 }) }"
-                    @click="commands.heading({ level: 4 })"
+                    :title="btn.title"
+                    :class="{
+                      active:
+                        btn.command.toggle &&
+                        isActive[btn.command.name](btn.command.attrs)
+                    }"
+                    @click="commands[btn.command.name](btn.command.attrs)"
                   >
-                    <b-icon icon="type-h1" />
-                  </b-btn>
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Подзаголовок"
-                    :class="{ active: isActive.heading({ level: 5 }) }"
-                    @click="commands.heading({ level: 5 })"
-                  >
-                    <b-icon icon="type-h2" />
-                  </b-btn>
-                </b-btn-group>
-
-                <b-btn-group size="sm" class="mx-2">
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Маркированный список"
-                    :class="{ active: isActive.bullet_list() }"
-                    @click="commands.bullet_list"
-                  >
-                    <b-icon icon="list-ul" />
-                  </b-btn>
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Нумерованный список"
-                    :class="{ active: isActive.ordered_list() }"
-                    @click="commands.ordered_list"
-                  >
-                    <b-icon icon="list-ol" />
-                  </b-btn>
-                </b-btn-group>
-
-                <b-btn-group size="sm">
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Отменить"
-                    @click="commands.undo"
-                  >
-                    <b-icon icon="arrow-counterclockwise" />
-                  </b-btn>
-                  <b-btn
-                    v-b-tooltip.hover
-                    title="Повторить"
-                    @click="commands.redo"
-                  >
-                    <b-icon icon="arrow-clockwise" />
+                    <b-icon :icon="btn.icon" />
                   </b-btn>
                 </b-btn-group>
               </b-col>
@@ -174,7 +92,84 @@ export default {
         onUpdate: ({ getJSON }) => {
           this.$emit('input', getJSON())
         }
-      })
+      }),
+      buttons: [
+        [
+          {
+            title: 'Абзац',
+            command: { name: 'paragraph', toggle: true },
+            icon: 'dash'
+          }
+        ],
+        [
+          {
+            title: 'Полужирный',
+            command: { name: 'bold', toggle: true },
+            icon: 'type-bold'
+          },
+          {
+            title: 'Курсив',
+            command: { name: 'italic', toggle: true },
+            icon: 'type-italic'
+          },
+          {
+            title: 'Подчеркнутый',
+            command: { name: 'underline', toggle: true },
+            icon: 'type-underline'
+          }
+        ],
+        [
+          {
+            title: 'Код',
+            command: { name: 'code', toggle: true },
+            icon: 'code-slash'
+          }
+        ],
+        [
+          {
+            title: 'Заголовок',
+            command: {
+              name: 'heading',
+              attrs: { level: 4 },
+              toggle: true
+            },
+            icon: 'type-h1'
+          },
+          {
+            title: 'Подзаголовок',
+            command: {
+              name: 'heading',
+              attrs: { level: 5 },
+              toggle: true
+            },
+            icon: 'type-h2'
+          }
+        ],
+        [
+          {
+            title: 'Маркированный список',
+            command: { name: 'bullet_list', toggle: true },
+            icon: 'list-ul'
+          },
+          {
+            title: 'Нумерованный список',
+            command: { name: 'ordered_list', toggle: true },
+            icon: 'list-ol'
+          }
+        ],
+        [
+          {
+            title: 'Отменить',
+            command: { name: 'undo' },
+            icon: 'arrow-counterclockwise'
+          },
+          {
+            title: 'Повторить',
+            command: { name: 'redo' },
+            icon: 'arrow-clockwise'
+          }
+        ]
+      ]
     }
   },
 
@@ -204,12 +199,6 @@ export default {
     color: #212529;
     background-color: #e0e0e0;
     border-color: #e0e0e0;
-  }
-}
-
-@media screen and (max-width: var(--breakpoint-md)) {
-  .second-icon-row {
-    margin-top: 0.25rem;
   }
 }
 </style>

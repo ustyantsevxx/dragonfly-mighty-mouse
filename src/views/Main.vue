53a4d496e3d37cb97185a6cd327efa88db2c88df
Timofei <tuistssip@gmail.com>
2020-05-13 01:28:41 +0500

    change main page

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,10 +3,25 @@
     <b-container>
       <b-row>
         <b-col>
-          <b-jumbotron
-            header="main page"
-            lead="app's desc goes here"
-          ></b-jumbotron>
+          <b-jumbotron>
+            <template #header>
+              <span class="font-weight-bold">Dragonfly</span>
+              v0.1.0-alpha
+            </template>
+            <template #lead>
+              Приложение для контроля успеваемости студентов в университете.
+            </template>
+            <hr />
+            <b-btn
+              href="https://github.com/raritetmolodoy/dragonfly-mighty-mouse"
+              target="_blank"
+              size="lg"
+              variant="light"
+            >
+              <img class="mr-3" src="@/assets/gh-logo.png" />
+              Исходный код
+            </b-btn>
+          </b-jumbotron>
         </b-col>
       </b-row>
     </b-container>
